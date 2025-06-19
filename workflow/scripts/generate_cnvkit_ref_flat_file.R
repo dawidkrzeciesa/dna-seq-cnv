10a7ecd2c392653621ecba4a0ab0fdaf6ce64ade
David Laehnemann <david.laehnemann@hhu.de>
2025-06-19 18:13:32 +0200

    fix: try fixing the ref_flat.tsv formatting, but positions still seem off

--- workflow/scripts/generate_cnvkit_ref_flat_file.R
+++ workflow/scripts/generate_cnvkit_ref_flat_file.R
@@ -9,6 +9,9 @@ library(tidyverse)
 multi_core_cluster <- new_cluster(snakemake@threads)
 
 partitioned_input = read_tsv(snakemake@input[["table"]]) |>
+  arrange(
+    exon_chrom_start
+  ) |>
   group_by(
     external_gene_name,
     ensembl_transcript_id,
@@ -22,6 +25,10 @@ partitioned_input = read_tsv(snakemake@input[["table"]]) |>
   )
 
 ref_flat <- partitioned_input |>
+  mutate(
+    # make cds positions absolute on the chromosome, vs. relative to the transcript
+    across(c(cds_start, cds_end), ~ .x + exon_chrom_start - 1)
+  ) |>
   summarize(
     cds_start = min(cds_start, na.rm = TRUE),
     cds_end = max(cds_end, na.rm = TRUE),
@@ -38,10 +45,15 @@ ref_flat <- partitioned_input |>
     # to mirror the refFlat.txt format, we add trailing commas to
     # lists of exon start and end positions
     across(c(exon_starts, exon_ends), ~ stringr::str_c(.x, ",")),
-    # if no cds exists for a (non-coding) transcript, we want an NA
-    # value that will be written as an empty value below
-    cds_start = dplyr::na_if(cds_start, Inf),
-    cds_end = dplyr::na_if(cds_end, -Inf)
+    # always put something into the cds start and end:
+    # * the min() and max() return Inf / -Inf for entries with only NA
+    # * refFlat.txt files seem to have the transcript_end position in
+    # both (without something in there, the cnvkit batch format sniffing
+    # regex for this fails)
+    across(
+      c(cds_start, cds_end),
+      ~ dplyr::if_else(is.infinite(.x), transcript_end, .x)
+    )
   ) |>
   select(
     external_gene_name,
